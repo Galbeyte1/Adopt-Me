82b7f1fe42dbf544576115721fae62b7fe174842
Galbeyte <s.galbeyte@gmail.com>
2023-06-27 23:52:06 -0500

    Beginning to convert App to consume froom Pets API. Search Param componenet added. Updated esl lint to include react hooks.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { createRoot } from "react-dom";
-import Pet from "./Pet";
-
+import { createRoot } from "react-dom/client";
+import SearchParams from "../SearchParams";
 // Your code is going to go here
 // always capitalize components
 const App = () => {
-  <div>
-    <h1>Adopt Me!</h1>
-    <Pet name="Luna" animal="dog" breed="Havanese" />
-    <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-    <Pet name="Doink" animal="cat" breed="Mixed" />
-    <div />
-  </div>;
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
 // We have a function but we aren't using it. Lets use it here.
