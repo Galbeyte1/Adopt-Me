273ec3f4c59a656a9871d27e908684209d87ff27
Galbeyte <s.galbeyte@gmail.com>
2023-06-27 22:07:46 -0500

    Converting to JSX more readable equivalent of components. Linting esl configuration.

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,21 @@
-import React from "react";
 import { createRoot } from "react-dom";
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import Pet from "./Pet";
 
 // Your code is going to go here
 // always capitalize components
 const App = () => {
-  return React.createElement(
-    "div",
-    {}, // whatever atributes that we are handing out.
-    [
-      React.createElement("h1", {}, "Adopt Me!"),
-      // Information flowing from App to the Pet component, through properties
-      React.createElement(Pet, {
-        animal: "Dog",
-        name: "Luna",
-        breed: "Havanese",
-      }),
-      React.createElement(Pet, {
-        animal: "Bird",
-        name: "Pepper",
-        breed: "Cockatiel",
-      }),
-      React.createElement(Pet, {
-        animal: "Cay",
-        name: "Doink",
-        breed: "Mixed",
-      }),
-    ]
-  );
+  <div>
+    <h1>Adopt Me!</h1>
+    <Pet name="Luna" animal="dog" breed="Havanese" />
+    <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+    <Pet name="Doink" animal="cat" breed="Mixed" />
+    <div />
+  </div>;
 };
 
 // We have a function but we aren't using it. Lets use it here.
 // We essentially created a class but haven't instantiated it
 const container = document.getElementById("root");
 const root = createRoot(container); // Rendering to the DOM
-root.render(React.createElement(App)); // Notice: We are using create element again here
+root.render(<App />); // Notice: We are using create element again here
 // We are creating components...made up of other components
